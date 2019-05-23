b05c7c4a3185aa9c102cc9b88a1105da37dbd101
Denis Korytkin <denis.korytkin@nextiva.com>
2019-05-23 19:08:19 +0300

    add test examples

---
+++ tests/factory.py
@@ -0,0 +1,61 @@
+
+from factory import Factory, Faker, SubFactory
+
+from test_app.models.db import accounts, orders, Role, products
+
+
+class MyFactory(Factory):
+
+    @classmethod
+    def _build(cls, model_class, *args, **kwargs):
+        return model_class.insert().values(*args, **kwargs)
+
+
+class AccountAdmin(MyFactory):
+
+    class Meta:
+        model = accounts
+
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    email = Faker('email')
+    role = Role.admin
+
+
+class AccountManager(MyFactory):
+
+    class Meta:
+        model = accounts
+
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    email = Faker('email')
+    role = Role.manager
+
+
+class AccountUser(MyFactory):
+
+    class Meta:
+        model = accounts
+
+    first_name = Faker('first_name')
+    last_name = Faker('last_name')
+    email = Faker('email')
+    role = Role.user
+
+
+class UserOrder(MyFactory):
+
+    class Meta:
+        model = orders
+
+    account_id = SubFactory(AccountUser)
+
+
+class Product(MyFactory):
+
+    class Meta:
+        model = products
+
+    name = Faker('name')
+    price = Faker('pyint')
